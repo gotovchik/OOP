ae5a306f804b3e73df90b4772aa4401e3feeaa6c
Igor Gotovchik <gotovchik.igor@yandex.ru>
2022-12-14 20:56:41 +0300

    dev homework_0

--- homework_0/App.java
+++ homework_0/App.java
@@ -5,11 +5,13 @@ public class App {
         HotDrink cappuccino = new HotDrink(1, "Cappuccino", 0.2, 90, 120);
         HotDrink tea = new HotDrink(2, "Black Tea", 0.3, 75, 40);
         HotDrink espresso = new HotDrink(3, "Espresso", 0.1, 100, 90);
+        Drink pepsi = new Drink(4, "Pepsi", 0.5, 70);
 
         HotDrinksVendingMachine machine = new HotDrinksVendingMachine();
         machine.addDrink(cappuccino);
         machine.addDrink(tea);
         machine.addDrink(espresso);
+        machine.addDrink(pepsi);
 
         machine.showDrinks(); // Хотим посмотреть какие напитки есть в автомате
         machine.getDrink(espresso); //Покупаем напиток
